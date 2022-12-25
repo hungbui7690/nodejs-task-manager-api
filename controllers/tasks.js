5b1b60a852f8edbfa82e3c7f787be88930ce96a8
hungbui7690 <hungbui7690@gmail.com>
2022-12-24 19:13:56 -0700

    deleteTask + updateTask controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,5 @@
 const Task = require('../models/Task')
 
-// (2)
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({})
@@ -10,17 +9,19 @@ const getAllTasks = async (req, res) => {
   }
 }
 
-// (3)
 const getTask = async (req, res) => {
-  const { id } = req.params
-  const task = await Task.findOne({ _id: id })
+  try {
+    const { id } = req.params
+    const task = await Task.findOne({ _id: id })
 
-  if (!task) return res.status(404).json({ msg: `No task with id ${id}` })
+    if (!task) return res.status(404).json({ msg: `No task with id ${id}` })
 
-  res.json({ task })
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
-// (1) setup try/catch
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body)
@@ -29,11 +30,38 @@ const createTask = async (req, res) => {
     res.status(500).json({ msg: error })
   }
 }
+
+// (2) end >> postman >> pic: postman-update-delete
 const updateTask = async (req, res) => {
-  res.json({ id: req.params.id })
+  try {
+    const { id } = req.params
+
+    // update: need id + body
+    const task = await Task.findOneAndUpdate({ _id: id }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+
+    if (!task) return res.status(404).json({ msg: `No task with id ${id}` })
+
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
+
+// (1)
 const deleteTask = async (req, res) => {
-  res.json({ id: req.params.id })
+  try {
+    const { id } = req.params
+    const task = await Task.findOneAndDelete({ _id: id })
+
+    if (!task) return res.status(404).json({ msg: `No task with id ${id}` })
+
+    res.json({ success: 'success' })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 module.exports = { getAllTasks, createTask, updateTask, deleteTask, getTask }
